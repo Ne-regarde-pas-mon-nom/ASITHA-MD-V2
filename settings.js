e15c57051354eb89fc1d9bb64ae907be640a13f7
Ne-regarde-pas-mon-nom <vegecraftastuces.@gmail.com>
2024-12-08 12:28:30 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=77BFEZpL#hZEyVjWl2Vnl2AqmvR6cBDrctuxqwb5yPCFZ_ikY9k8' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? '𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=KvQxnBDB#zSfYzuwgoTjCGk6J7jhp-dwSt_AEgyzAq1XqgMr-bQs' : process.env.SESSION_ID,
 PREFIX: process.env.PREFIX || '.' ,
 ALIVE_MSG: process.env.ALIVE_MSG === undefined ? "*Hello , I am alive now!!*" : process.env.ALIVE_MSG,
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS === undefined ?"true" : process.env.AUTO_READ_STATUS,
